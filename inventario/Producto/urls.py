d5450c4b31b35f55688b420964fe24be0faf8e51
AlvaroGonbri <gonzalezbriones.alvaro@outlook.com>
2025-04-23 22:48:45 -0400

    Se crea API para listar productos por ID o codigo de producto

--- inventario/Producto/urls.py
+++ inventario/Producto/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('producto', Clase1.as_view())
+    path('producto', Clase1.as_view()),
+    path('producto/<int:id>', Clase2.as_view())
 ]
--- inventario/Producto/views.py
+++ inventario/Producto/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from django.http.response import JsonResponse
 from .models import *
 from .serializers import *
+from django.http import Http404
+from http import HTTPStatus
 
 # Create your views here.
 
@@ -14,7 +16,24 @@ class Clase1(APIView):
         # select * from caterorias order by id desc
         data = Producto.objects.order_by('-cod_material').all()
         datos_json = CategoriaSerializer(data, many=True)
-        return Response(datos_json.data)
-        pass
+        return JsonResponse({"data":datos_json.data}, status=HTTPStatus.OK)
+        
 
+class Clase2(APIView):
 
+    def get(self, request, id):
+        try:
+            data = Producto.objects.get(cod_material=id)
+            return JsonResponse({
+                "data": {
+                    "cod_material": data.cod_material,
+                    "nom_producto": data.nom_producto,
+                    "cant_existencia": data.cant_existencia,
+                    "descripcion": data.descripcion,
+                    "stock_minimo": data.stock_minimo,
+                    "stock_maximo": data.stock_maximo,
+                    "categoria_id": data.categoria_id,
+                }
+            }, status=HTTPStatus.OK)
+        except Producto.DoesNotExist:
+            raise Http404()
